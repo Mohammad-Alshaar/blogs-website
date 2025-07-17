dc2cf605d0274e90cf923e3ee1932ae54ddfe5ac
Mohammad Alshaar <moalshaar225@gmail.com>
2025-07-17 17:17:18 +0300

    edit constants file

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,9 @@
 export const ARTICLE_PER_PAGE = 6;
 
-// export const DOMAIN = "http://localhost:3000";
-export const DOMAIN = "https://blogs-website-e5yf.vercel.app";
+export const DEVELOPMENT_DOMAIN = "http://localhost:3000";
+export const PRODUCTION_DOMAIN = "https://blogs-website-e5yf.vercel.app";
+
+export const DOMAIN =
+  process.env.NODE_ENV === "production"
+    ? PRODUCTION_DOMAIN
+    : DEVELOPMENT_DOMAIN;
