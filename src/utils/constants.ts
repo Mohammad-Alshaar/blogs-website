a12abde870e68410480eaa67915c95f071d970d9
Mohammad Alshaar <moalshaar225@gmail.com>
2025-07-17 15:43:34 +0300

    change domain

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
 export const ARTICLE_PER_PAGE = 6;
 
-export const DOMAIN = "http://localhost:3000";
+// export const DOMAIN = "http://localhost:3000";
+export const DOMAIN = "https://blogs-website-e5yf.vercel.app";
